a0de6618f978855054a416ac230f88457d281918
seladore <mbdesai@umich.edu>
2018-05-25 15:17:20 -0400

    fix logic

--- R/check_embargo_date.R
+++ R/check_embargo_date.R
@@ -12,12 +12,11 @@
 check_embargo_date <- function(metadata_dataset) {
 
   embargo_date <- unlist(metadata_dataset$field_wbddh_ds_embargo_date, use.names = FALSE)
-  embargo_date <- as.Date(embargo_date)
   dataset_nid <- unlist(metadata_dataset$nid, use.names = FALSE)
 
   if (is_blank(embargo_date)) {
     out <- list("dataset", dataset_nid, "check_embargo_date", "PASS", "there is no embargo date")
-  } else if (embargo_date >= Sys.Date()) {
+  } else if (as.Date(embargo_date) <= Sys.Date()) {
     out <- list("dataset", dataset_nid, "check_embargo_date", "FAIL", "embargo date has passed")
   } else {
     out <- list("dataset", dataset_nid, "check_embargo_date", "PASS", "embargo date hasn't passed")
--- tests/testthat/test-check_embargo_date.R
+++ tests/testthat/test-check_embargo_date.R
@@ -4,7 +4,7 @@ test_that("Flags passed date", {
   metadata <- list("nid" = "007", "field_wbddh_ds_embargo_date" = "1997-06-26 00:00:00")
   expect_equal(
     check_embargo_date(metadata),
-    list("dataset", "007", "check_missing", "FAIL", "embargo date has passed")
+    list("dataset", "007", "check_embargo_date", "FAIL", "embargo date has passed")
   )
 })
 
@@ -12,7 +12,7 @@ test_that("Ok with empty date", {
   metadata <- list("nid" = "007", "field_wbddh_ds_embargo_date" = "")
   expect_equal(
     check_embargo_date(metadata),
-    c("dataset", "007", "check_missing", "PASS", "there is no embargo date")
+    list("dataset", "007", "check_embargo_date", "PASS", "there is no embargo date")
   )
 })
 
@@ -20,6 +20,6 @@ test_that("Ok with future date", {
   metadata <- list("nid" = "007", "field_wbddh_ds_embargo_date" = "2027-06-26 00:00:00")
   expect_equal(
     check_embargo_date(metadata),
-    c("dataset", "007", "check_missing", "PASS", "embargo date hasn't passed")
+    list("dataset", "007", "check_embargo_date", "PASS", "embargo date hasn't passed")
   )
 })

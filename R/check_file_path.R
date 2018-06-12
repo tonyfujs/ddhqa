aa5b767ffeca216732227fb973bc5c17b52e6439
seladore <mbdesai@umich.edu>
2018-06-12 10:43:52 -0400

    remove local ref

--- R/check_file_path.R
+++ R/check_file_path.R
@@ -11,7 +11,7 @@
 check_file_path <- function(metadata_resource) {
 
   resource_nid <- unlist(metadata_resource[["nid"]], use.names = FALSE)
-  upload_path <- unlist(metadata[["field_upload"]], use.names = FALSE)
+  upload_path <- unlist(metadata_resource[["field_upload"]], use.names = FALSE)
 
   data_class_tid <- unlist(metadata_resource[["field_wbddh_data_class"]], use.names = FALSE)
   data_class_ui <- lovs[lovs$tid == data_class_tid & lovs$machine_name == "field_wbddh_data_class", ]$list_value_name

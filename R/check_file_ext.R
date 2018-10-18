208b43e3c31ea85fa6301c6be469573a1fb0b7ec
Yukun Wang <ywang32@worldbank.org>
2018-10-18 17:14:10 -0400

    # Re-touch on check_file_ext: add external link check;
    # Add unit test for check_file_ext()

--- R/check_file_ext.R
+++ R/check_file_ext.R
@@ -14,28 +14,59 @@
 #[ ] only select if populated in one of the following fields; `field_upload, field_link_api, field_link_remote_file`
 #[ ] deal with html in extension `is_blank(field_format) & !is_blank(file_ext)`
 
+# r_nid 93789
+# r_nid 100912
+
 check_file_ext <- function(metadata_resource,
                            lovs = ddhconnect::get_lovs()) {
 
   resource_nid <- unlist(metadata_resource[["nid"]], use.names = FALSE)
+
+  # Check the resource bucket
+  buckets <- metadata_resource[c('field_link_api', 'field_link_remote_file', 'field_upload')]
+  bucket <- names(which(!sapply(buckets, is_blank), arr.ind = TRUE)) # Extract the bucket where resource lies
+
+  if (!length(bucket)>0){ # No Resource link/file
+
+    out <- list("resource", resource_nid, "check_file_ext", "FAIL", "Missing resource link or file.")
+
+  } else if (bucket == 'field_link_api') { # Resource is an external link
+    out <- list("resource", resource_nid, "check_file_ext", "PASS", "Resource is external link.")
+  } else {
+
   field_format <- get_field_format(metadata_resource, lovs)
-  allowed_ext <- get_allowed_ext(field_format)
+  allowed_ext <- get_allowed_ext(field_format) # allowed ext for given field_fomat not listed in the look-up table, could be residual.
   allowed_collapsed <- glue::glue_collapse(allowed_ext, sep = ",")
 
   path <- dkanr::get_resource_url(metadata_resource)
   file_ext <- get_file_ext(path)
 
-  if (is_blank(allowed_ext) & is_blank(file_ext)) {
-    out <- list("resource", resource_nid, "check_file_ext", "PASS", "Both are blank")
-  } else if (is_blank(allowed_ext) & !is_blank(file_ext)) {
-    out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format is missing, value should take a {file_ext} extenstion"))
-  } else if (!is_blank(allowed_ext) & is_blank(file_ext)) {
-    out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The resource path is expected to take {allowed_collapsed} extension(s)"))
-  } else if (file_ext == allowed_ext | file_ext %in% allowed_ext) {
-    out <- list("resource", resource_nid, "check_file_ext", "PASS", glue::glue("The field_format's ({field_format}) allowed types match the resource's file ext ({file_ext})"))
-  } else {
-    out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format's ({field_format}) allowed types ({allowed_collapsed}) do not match the resource's file ext ({file_ext})"))
+  # if (is_blank(allowed_ext) & is_blank(file_ext)) {
+  #   out <- list("resource", resource_nid, "check_file_ext", "PASS", "Both are blank")
+  # } else if (is_blank(allowed_ext) & !is_blank(file_ext)) {
+  #   out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format is missing, value should take a {file_ext} extenstion"))
+  # } else if (!is_blank(allowed_ext) & is_blank(file_ext)) {
+  #   out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The resource path is expected to take {allowed_collapsed} extension(s)"))
+  # } else if (file_ext == allowed_ext | file_ext %in% allowed_ext) {
+  #   out <- list("resource", resource_nid, "check_file_ext", "PASS", glue::glue("The field_format's ({field_format}) allowed types match the resource's file ext ({file_ext})"))
+  # } else {
+  #   out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format's ({field_format}) allowed types ({allowed_collapsed}) do not match the resource's file ext ({file_ext})"))
+  # }
+
+ if (!is_blank(allowed_ext) & is_blank(file_ext)) {
+
+    out <- list("resource", resource_nid, "check_file_ext", "FAIL",
+                glue::glue("The resource path is expected to take {allowed_collapsed} extension(s)"))
+
+  } else if (!is_blank(allowed_ext) & !is_blank(file_ext) & !(file_ext %in% allowed_ext)) {
+
+    out <- list("resource", resource_nid, "check_file_ext", "FAIL",
+                glue::glue("The field_format's ({field_format}) allowed types ({allowed_collapsed}) do not match the resource's file ext ({file_ext})"))
+  } else  {
+
+    out <- list("resource", resource_nid, "check_file_ext", "PASS", "Resource extension format matched/No pre-existing requirement on given format.")
   }
+}
 
   return(out)
 }
---
+++ tests/testthat/test-check_file_ext.R
@@ -0,0 +1,109 @@
+context("Tests check if resource file extension match")
+
+httptest::start_capturing(path = "./tests/testthat")
+dkanr::dkanr_setup(
+  url = "https://datacatalog.worldbank.org/",
+  username = Sys.getenv("ddh_username"),
+  password = Sys.getenv("ddh_password")
+)
+ddhconnect::get_metadata(nid = "140158")
+ddhconnect::get_lovs()
+httptest::stop_capturing()
+
+# if (bucket == 'field_link_api') { # Resource is an external link
+#   out <- list("resource", resource_nid, "check_file_ext", "PASS", "Resource is external link.")
+# } else if (!length(bucket)>0){ # No Resource link/file
+#
+#   out <- list("resource", resource_nid, "check_file_ext", "FAIL", "Missing resource link or file.")
+#
+# } else {
+#
+#   field_format <- get_field_format(metadata_resource, lovs)
+#   allowed_ext <- get_allowed_ext(field_format) # allowed ext for given field_fomat not listed in the look-up table, could be residual.
+#   allowed_collapsed <- glue::glue_collapse(allowed_ext, sep = ",")
+#
+#   path <- dkanr::get_resource_url(metadata_resource)
+#   file_ext <- get_file_ext(path)
+#
+#   if (is_blank(allowed_ext) & is_blank(file_ext)) {
+#     out <- list("resource", resource_nid, "check_file_ext", "PASS", "Both are blank")
+#   } else if (is_blank(allowed_ext) & !is_blank(file_ext)) {
+#     out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format is missing, value should take a {file_ext} extenstion"))
+#   } else if (!is_blank(allowed_ext) & is_blank(file_ext)) {
+#     out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The resource path is expected to take {allowed_collapsed} extension(s)"))
+#   } else if (file_ext == allowed_ext | file_ext %in% allowed_ext) {
+#     out <- list("resource", resource_nid, "check_file_ext", "PASS", glue::glue("The field_format's ({field_format}) allowed types match the resource's file ext ({file_ext})"))
+#   } else {
+#     out <- list("resource", resource_nid, "check_file_ext", "FAIL", glue::glue("The field_format's ({field_format}) allowed types ({allowed_collapsed}) do not match the resource's file ext ({file_ext})"))
+#   }
+# }
+
+httptest::with_mock_api({
+  test_that("The resource is a external link", {
+    metadata <- ddhconnect::get_metadata(nid = "93789")
+    lovs <- ddhconnect::get_lovs()
+    expect_equal(
+      check_file_ext(metadata, lovs),
+      list("resource", "93789", "check_file_ext", "PASS", "Resource is external link.")
+    )
+  })
+})
+
+
+httptest::with_mock_api({
+  test_that("Resource file/link extension matched or no pre-existing requirement", {
+    metadata <- ddhconnect::get_metadata(nid = "93789")
+    metadata$field_link_api <- list()
+    metadata$field_link_remote_file <- list()
+    metadata$field_upload <- list('und'=list(list( 'uri' = "test_add_data.csv" )))
+    metadata[['field_format']] <- list('und'=list(list('tid'= "14")))
+    lovs <- ddhconnect::get_lovs()
+    expect_equal(
+      check_file_ext(metadata, lovs),
+      list("resource", "93789", "check_file_ext", "PASS",  "Resource extension format matched/No pre-existing requirement on given format.")
+    )
+  })
+})
+
+httptest::with_mock_api({
+  test_that("Resource link or file missing", {
+    metadata <- ddhconnect::get_metadata(nid = "93789")
+    metadata[c('field_link_api', 'field_link_remote_file', 'field_upload')] <- NA
+    lovs <- ddhconnect::get_lovs()
+    expect_equal(
+      check_file_ext(metadata, lovs),
+      list("resource", "93789", "check_file_ext", "FAIL", "Missing resource link or file.")
+    )
+  })
+})
+
+
+httptest::with_mock_api({
+  test_that("Resource file/link extension blank", {
+    metadata <- ddhconnect::get_metadata(nid = "93789")
+    metadata$field_link_api <- list()
+    metadata$field_link_remote_file <- list()
+    metadata$field_upload <- list('und'=list(list( 'uri' = "https://geowb.worldbank.org/portal/sharing/servers/bb6bb3bb54c949c7ba2720d8ce6b33b6/rest/services/MYS_Demographics_and_Boundaries/MapServer")))
+    metadata[['field_format']] <- list('und'=list(list('tid'= "14")))
+    lovs <- ddhconnect::get_lovs()
+    expect_equal(
+      check_file_ext(metadata, lovs),
+      list("resource", "93789", "check_file_ext", "FAIL",  glue::glue("The resource path is expected to take {allowed_collapsed} extension(s)"))
+    )
+  })
+})
+
+httptest::with_mock_api({
+  test_that("Resource file/link extension do not match as required", {
+    metadata <- ddhconnect::get_metadata(nid = "93789")
+    metadata$field_link_api <- list()
+    metadata$field_link_remote_file <- list()
+    metadata$field_upload <- list('und'=list(list( 'uri' = "test_add_data.csv" )))
+    metadata[['field_format']] <- list('und'=list(list('tid'= "1369")))
+    lovs <- ddhconnect::get_lovs()
+    expect_equal(
+      check_file_ext(metadata, lovs),
+      list("resource", "93789", "check_file_ext", "FAIL", glue::glue("The field_format's ({field_format}) allowed types ({allowed_collapsed}) do not match the resource's file ext ({file_ext})"))
+    )
+  })
+})
